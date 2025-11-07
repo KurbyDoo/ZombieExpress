fa2293592ee341c7e6465c51ca83a042a609768b
Leobstere <jerrryzh163@gmail.com>
2025-11-07 01:04:50 -0500

    Trying to implement btBvhTriangleMeshShape on the chunks instead of individual blocks. The current idea is using a new class called PhysicsSystem and call it somewhere inbetween GameMeshBuilder and ChunkLoader to generate the collision detection. Then add the shape to a CollisionWorld.

--- core/src/main/java/presentation/view/GameView.java
+++ core/src/main/java/presentation/view/GameView.java
@@ -16,6 +16,11 @@ import application.use_cases.PlayerMovement.PlayerMovementInputBoundary;
 import application.use_cases.PlayerMovement.PlayerMovementInteractor;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.bullet.Bullet;
+import com.badlogic.gdx.physics.bullet.collision.btCollisionDispatcher;
+import com.badlogic.gdx.physics.bullet.collision.btCollisionWorld;
+import com.badlogic.gdx.physics.bullet.collision.btDbvtBroadphase;
+import com.badlogic.gdx.physics.bullet.collision.btDefaultCollisionConfiguration;
 import io.github.testlibgdx.ChunkLoader;
 import infrastructure.rendering.GameMeshBuilder;
 import infrastructure.rendering.ObjectRenderer;
@@ -50,6 +55,8 @@ public class GameView implements Viewable {
     private ChunkGenerationInteractor chunkGenerationUseCase;
     private Player player;
 
+    public btCollisionWorld collisionWorld;
+
     private float accumulator;
 
     private Stage uiStage;
@@ -67,6 +74,16 @@ public class GameView implements Viewable {
 
     @Override
     public void createView() {
+
+        // need to initialize before any BulletPhysics related calls
+        Bullet.init();
+        // initialize collisionWorld
+        btDefaultCollisionConfiguration config = new btDefaultCollisionConfiguration();
+        btCollisionDispatcher dispatcher = new btCollisionDispatcher(config);
+        btDbvtBroadphase broadphase = new btDbvtBroadphase();
+        collisionWorld = new btCollisionWorld(dispatcher, broadphase, config);
+
+        Vector3 startingPosition = new Vector3(0, 200f, 0);
         Vector3 startingPosition = new Vector3(0, 16f, 0);
         player = new Player(startingPosition);
 
