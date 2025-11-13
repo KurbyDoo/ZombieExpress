a63e83eaa60f251acc6dd5523ee9760d7c18dd27
rachphm <rachel.phm25@gmail.com>
2025-11-13 14:08:34 -0500

    Organize code to follow CA

--- core/src/main/java/infrastructure/rendering/ObjectRenderer.java
+++ core/src/main/java/infrastructure/rendering/ObjectRenderer.java
@@ -9,13 +9,10 @@ import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.utils.DefaultShaderProvider;
-
-import physics.CollisionHandler;
-import physics.GameMesh;
-
-import net.mgsx.gltf.loaders.gltf.GLTFLoader; // Keep from Left for GLTF functionality
+import com.badlogic.gdx.math.Vector3;
+import net.mgsx.gltf.loaders.gltf.GLTFLoader;
 import net.mgsx.gltf.scene3d.scene.Scene;
-import net.mgsx.gltf.scene3d.scene.SceneAsset; // Keep from Left for GLTF functionality
+import net.mgsx.gltf.scene3d.scene.SceneAsset;
 import net.mgsx.gltf.scene3d.scene.SceneManager;
 
 import java.util.ArrayList;
@@ -28,19 +25,17 @@ public class ObjectRenderer {
 
     public PerspectiveCamera camera;
     public ModelBatch modelBatch;
+    public List<ModelInstance> models = new ArrayList<>();
 
-    // RENDER LISTS & HANDLERS (Prioritizing Right's structure)
-    public List<GameMesh> models = new ArrayList<>();
+    // testing with a zombie instance
+    public ModelInstance zombieInstance;
+    // add scene attributes (to load models)
     private SceneManager sceneManager;
-    public CollisionHandler colHandler;
+//    private SceneAsset sceneAsset;
+//    private Scene scene;
 
-    // GLTF LOADING (From Left, to be used by the entity system)
-    private SceneAsset zombieSceneAsset;
-    public ModelInstance zombieInstance; // Keep for temporary access/testing if needed
-
-    // ASYNCHRONOUS QUEUES (Using GameMesh)
-    public BlockingQueue<GameMesh> toAdd = new LinkedBlockingQueue<>();
-    public BlockingQueue<GameMesh> toRemove = new LinkedBlockingQueue<>();
+    public BlockingQueue<ModelInstance> toAdd = new LinkedBlockingQueue<>();
+    public BlockingQueue<ModelInstance> toRemove = new LinkedBlockingQueue<>();
 
     public ObjectRenderer(PerspectiveCamera camera) {
         environment = new Environment();
@@ -48,119 +43,99 @@ public class ObjectRenderer {
         environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
 
         modelBatch = new ModelBatch();
-        this.camera = camera;
-        this.colHandler = colHandler;
 
-        // Set up Scene Manager (From Right)
+        this.camera = camera;
+
+        //set up scene
+        sceneAsset = new GLTFLoader().load(Gdx.files.internal("models/model.gltf"));
+        scene = new Scene(sceneAsset.scene);
         sceneManager = new SceneManager();
         sceneManager.setShaderProvider(new DefaultShaderProvider());
+//        sceneManager.addScene(scene);
         sceneManager.setCamera(camera);
         sceneManager.setAmbientLight(1f);
-
-        // Load the zombie GLTF asset (From Left)
-        // NOTE: The asset itself is loaded, but not immediately added to the sceneManager,
-        // as the EntityController will handle scene additions.
-        zombieSceneAsset = new GLTFLoader().load(Gdx.files.internal("models/model.gltf"));
     }
 
-    public void add(GameMesh obj) {
-        toAdd.add(obj);
-        colHandler.add(obj); // Add to collision handler (from Right)
+    public void add(ModelInstance modelInstance) {
+        toAdd.add(modelInstance);
     }
 
-    public void remove(GameMesh obj) { // Added from Left, using GameMesh
-        if (obj != null) {
-            toRemove.add(obj);
+    public void remove(ModelInstance modelInstance) {
+        if (modelInstance != null) {
+            toRemove.add(modelInstance);
         }
     }
 
-    public void addToSceneManager(Scene scene) { // From Right
-        System.out.println("Model added to scene.");
-        sceneManager.addScene(scene);
-    }
-
-    // Kept from Left to allow the EntityController to set the instance
     public void addZombieInstance(ModelInstance zombieInstance) {
         this.zombieInstance = zombieInstance;
     }
 
+    public void addZombieToScene(Scene scene) {
+        System.out.println("Zombie added to scene.");
+        sceneManager.addScene(scene);
+    }
+
     private void updateRenderList() {
-        GameMesh instance;
+        ModelInstance instance;
 
         // Add new models
         while ((instance = toAdd.poll()) != null) {
             models.add(instance);
         }
-
-        // Remove and dispose old models (From Middle/Right logic, adapted for GameMesh)
-        while ((instance = toRemove.poll()) != null) {
-            models.remove(instance);
-
-            instance.dispose();
-            instance.modelDispose();
-
-            colHandler.remove(instance);
-        }
     }
 
-
-    public void render(Float deltaTime) {
+    public void render() {
         updateRenderList();
-
-        // OpenGL State configuration (from Right)
-        Gdx.gl.glEnable(GL20.GL_DEPTH_TEST);
-        Gdx.gl.glEnable(GL20.GL_CULL_FACE);
-        Gdx.gl.glCullFace(GL20.GL_BACK);
 
         Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
-        // Render scene manager (from Right)
+        // Move zombie
+        //zombieInstance.transform.translate(deltaTime*2, 0f, 0f);
+        if (zombieInstance != null) {
+            Vector3 zombiePos = zombieInstance.transform.getTranslation(new Vector3());
+            Vector3 direction = new Vector3(playerPos).sub(zombiePos).nor();
+            float speed = 2f;
+            zombiePos.add(direction.scl(deltaTime * speed));
+            zombieInstance.transform.setTranslation(zombiePos);
+        }
+
+        // render zombie model
+//        Vector3 zombiePos = scene.modelInstance.transform.getTranslation(new Vector3());
+//        Vector3 direction = new Vector3(playerPos).sub(zombiePos).nor();
+//        float speed = 2f;
+//        zombiePos.add(direction.scl(deltaTime * speed));
+//        scene.modelInstance.transform.setTranslation(zombiePos);
+//        //scene.modelInstance.transform.setToTranslation(0, 14f, 0f);
         sceneManager.update(deltaTime);
         sceneManager.render();
 
-        // Movement Update (from Right)
-        final float delta = Math.min(1f / 30f, Gdx.graphics.getDeltaTime());
-        for (GameMesh obj : models){
-            if (obj.moving){
-                obj.transform.trn(0f, -delta, 0f);
-                obj.body.setWorldTransform(obj.transform);
-            }
-        }
-
-        colHandler.checkCollision(); // Check collisions (from Right)
-
         modelBatch.begin(camera);
-        for (GameMesh obj : models) {
-            modelBatch.render(obj, environment);
+        for (ModelInstance modelInstance : models) {
+            modelBatch.render(modelInstance, environment);
         }
+        //render zombie
+        modelBatch.render(zombieInstance, environment);
 
-        // Render the zombieInstance (From Left - if it exists, for testing)
-        if (zombieInstance != null) {
-            modelBatch.render(zombieInstance, environment);
-        }
         modelBatch.end();
     }
 
     public void dispose() {
-        for (GameMesh obj: models){
-            obj.dispose(); // Comprehensive GameMesh dispose
-            obj.modelDispose(); // Dispose model resources (from Right)
+        // Dispose all models currently in the list
+        for (ModelInstance modelInstance : models) {
+            if (modelInstance.model != null) {
+                modelInstance.model.dispose();
+            }
         }
         models.clear();
 
-        // Drain queues and dispose any remaining (from Left)
+        // Drain queues and dispose any remaining
         updateRenderList();
         toAdd.clear();
         toRemove.clear();
 
-        colHandler.dispose(); // Dispose collision handler (from Right)
         modelBatch.dispose();
-        sceneManager.dispose(); // Dispose scene manager (from Right)
-
-        // Dispose GLTF asset if needed
-        if (zombieSceneAsset != null) {
-            zombieSceneAsset.dispose(); // From Left
-        }
+        sceneManager.dispose();
+//        sceneAsset.dispose();
     }
 }
