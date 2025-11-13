c53515cd9814aec3dcda02ea0835bca55d09041e
KurbyDoo <106122181+KurbyDoo@users.noreply.github.com>
2025-11-13 01:36:02 -0500

    Fix game crash

--- core/src/main/java/infrastructure/rendering/ChunkMeshData.java
+++ core/src/main/java/infrastructure/rendering/ChunkMeshData.java
@@ -7,18 +7,21 @@ import com.badlogic.gdx.physics.bullet.collision.btTriangleMesh;
 import io.github.testlibgdx.GameObject;
 
 public class ChunkMeshData {
-    final public ModelInstance model;
-    final public btTriangleMesh triangle;
-    final public btBvhTriangleMeshShape shape;
+    final private Model model;
+    final private ModelInstance instance;
+    final private btTriangleMesh triangle;
+    final private btBvhTriangleMeshShape shape;
 //    public GameObject gameObject = null;
-    public ChunkMeshData(){
-        model = null;
-        triangle = null;
-        shape = null;
-    }
+//    public ChunkMeshData(){
+//        model = null;
+//        triangle = null;
+//        shape = null;
+//    }
 
-    public ChunkMeshData(ModelInstance model, btTriangleMesh triangle, btBvhTriangleMeshShape shape){
+    public ChunkMeshData(Model model, btTriangleMesh triangle, btBvhTriangleMeshShape shape){
+        assert model != null;
         this.model = model;
+        this.instance = new ModelInstance(model);
         this.triangle = triangle;
         this.shape = shape;
     }
@@ -29,10 +32,25 @@ public class ChunkMeshData {
 
 //    public GameObject getGameObject(){return this.gameObject;}
 
-    public ModelInstance getModel(){return this.model;}
+    /**
+     * WE MUST DISPOSE WHEN DE-RENDERING UNLOADING A CHUNK
+     */
+    public void dispose() {
+        model.dispose();
+        triangle.dispose();
+        shape.dispose();
+    }
 
-    public btTriangleMesh getTriangleMesh(){return this.triangle;}
+    public ModelInstance getModel() {
+        return this.instance;
+    }
 
-    public btBvhTriangleMeshShape getBvhTriangle(){return this.shape;}
+    public btTriangleMesh getTriangleMesh() {
+        return this.triangle;
+    }
+
+    public btBvhTriangleMeshShape getBvhTriangle() {
+        return this.shape;
+    }
 
 }
