1f21ec2c571b74827df37e860ae64e7361edfcb1
Emanon-tt <tangmeiyitiang@gmail.com>
2025-11-13 14:25:57 -0500

    Roughly completed the LoginController, need further test

--- core/src/main/java/interface_adapter/LoginController.java
+++ core/src/main/java/interface_adapter/LoginController.java
@@ -1,4 +1,13 @@
 package interface_adapter;
+import UseCases.Login.LoginInputBoundary;
 
 public class LoginController {
+    private final UseCases.Login.LoginInputBoundary interactor;
+
+    public LoginController(UseCases.Login.LoginInputBoundary interactor) {
+        this.interactor = interactor;
+    }
+    public void login(String email, String password) {
+        interactor.login(email, password);
+    }
 }
