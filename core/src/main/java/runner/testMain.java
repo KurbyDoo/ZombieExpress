dea6fa5eadf1a56b79ae043168af37df01d48916
Emanon-tt <tangmeiyitiang@gmail.com>
2025-11-16 15:11:25 -0500

    Finish basic login UI and mock data now working - Built the basic Swing LoginVieew - Implemented the related functions in LoginPresenter and LoginViewModel - testMain now can show successful UI update when using mock email and password
    --------
    next step:
    I need start researching and building the Sign up  system and also try to connect the data with real API

--- core/src/main/java/runner/testMain.java
+++ core/src/main/java/runner/testMain.java
@@ -1,7 +1,17 @@
 package runner;
 
+import UseCases.Login.*;
+import interface_adapter.*;
+import data_access.FirebaseAuthManager;
+import presentation.view.LoginView;
+
 public class testMain{
     public static void main(String[] args) {
-
+    LoginViewModel viewModel = new LoginViewModel();
+    LoginOutputBoundary presenter = new LoginPresenter(viewModel);
+    LoginDataAccessInterface dataAccess = new FirebaseAuthManager();
+    LoginInputBoundary interactor = new LoginInteractor(dataAccess, presenter);
+    LoginController controller = new LoginController(interactor);
+    new LoginView(controller, viewModel);
     }
 }
