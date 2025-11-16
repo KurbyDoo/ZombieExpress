35e47d12a700742f9797dd13dcef8ae684f29479
rachphm <rachel.phm25@gmail.com>
2025-11-16 18:51:17 -0500

    Rebase with Add zombieGeneration and RenderZombie use cases

--- core/src/main/java/application/use_cases/EntityGeneration/EntityGenerationInputData.java
+++ core/src/main/java/application/use_cases/EntityGeneration/EntityGenerationInputData.java
@@ -1,11 +1,4 @@
 package application.use_cases.EntityGeneration;
 
-import domain.entities.Zombies;
-
 public class EntityGenerationInputData {
-    private final Zombies zombie;
-
-    public EntityGenerationInputData() {
-        this.zombie = new Zombies();
-    }
 }
--- core/src/main/java/application/use_cases/EntityGeneration/EntityGenerationInteractor.java
+++ core/src/main/java/application/use_cases/EntityGeneration/EntityGenerationInteractor.java
@@ -1,28 +1,43 @@
 package application.use_cases.EntityGeneration;
 
-import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector3;
+import domain.entities.ZombieStorage;
 import domain.entities.Zombie;
-import infrastructure.rendering.ObjectRenderer;
-import net.mgsx.gltf.loaders.gltf.GLTFLoader;
-import net.mgsx.gltf.scene3d.scene.Scene;
-import net.mgsx.gltf.scene3d.scene.SceneAsset;
 
 public class EntityGenerationInteractor implements EntityGenerationInputBoundary{
-    private Zombie zombie;
-    private Scene scene;
-    private SceneAsset sceneAsset;
-    private ObjectRenderer objectRenderer;
+    // Makes changes to the zombies in the storage
+    // makes isRendered true
 
-    public EntityGenerationInteractor(ObjectRenderer objectRenderer) {
-        System.out.println("EntityGenerationInteractor initialized.");
-        this.objectRenderer = objectRenderer;
-        sceneAsset = new GLTFLoader().load(Gdx.files.internal("models/model.gltf"));
-        scene = new Scene(sceneAsset.scene);
+    private ZombieStorage zombieStorage;
+
+    public EntityGenerationInteractor(ZombieStorage zombieStorage) {
+        this.zombieStorage = zombieStorage;
     }
 
     @Override
     public void execute(EntityGenerationInputData inputData) {
-        System.out.println("EntityGenerationInteractor executed.");
-        objectRenderer.addZombieToScene(scene);
+        // generate a new zombie at position 0,0,0
+        Vector3 position = new Vector3(0f, 0f, 0f);
+        Zombie zombie = new Zombie(position);
+        //zombie.setRendered(true);
+        zombieStorage.addZombie(zombie);
+
+        }
     }
-}
+//    private Scene scene;
+//    private SceneAsset sceneAsset;
+//    private final ObjectRenderer objectRenderer;
+//
+//    public EntityGenerationInteractor(ObjectRenderer objectRenderer) {
+//        System.out.println("EntityGenerationInteractor initialized.");
+//        this.objectRenderer = objectRenderer;
+//    }
+//
+//    @Override
+//    public void execute(EntityGenerationInputData inputData) {
+//        System.out.println("EntityGenerationInteractor executed.");
+//        SceneAsset sceneAsset = new GLTFLoader().load(Gdx.files.internal("models/model.gltf"));
+//        Scene scene = new Scene(sceneAsset.scene);
+//        objectRenderer.addZombieToScene(scene);
+//    }
+
