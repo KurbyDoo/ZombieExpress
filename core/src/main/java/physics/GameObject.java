5c682a436aeb2e31bd4f7f24ed2b9a12d0a147f6
Leobstere <jerrryzh163@gmail.com>
2025-11-17 06:19:31 -0500

    Rebase with: Fixed not rendering issue - something wrong with my ContactListener implementation now resolved.
    Restructored GameObject to fit the context of the game.

--- core/src/main/java/physics/GameObject.java
+++ core/src/main/java/physics/GameObject.java
@@ -13,15 +13,22 @@ public class GameObject extends ModelInstance {
 
     public final btCollisionObject body;
     public boolean moving = true;
-    public String id;
-    public GameObject(Model model, String node, btCollisionShape shape){
-        super(model, node);
-        this.id = node;
 
+    public static int COUNTER;
+    public int id;
+
+    public GameObject(Model model, btCollisionShape shape){
+        super(model);
+        this.model = model;
         this.shape = shape;
+
+        COUNTER++;
+        id += COUNTER;
 
         body = new btCollisionObject();
         body.setCollisionShape(shape);
+
+
     }
 
     public void dispose () {
@@ -29,25 +36,10 @@ public class GameObject extends ModelInstance {
         shape.dispose();
     }
 
-//    static class Constructor implements Disposable {
-//        public final Model model;
-//        public final String node;
-//        public final btCollisionShape shape;
-//
-//        public Constructor (Model model, String node, btCollisionShape shape) {
-//            this.model = model;
-//            this.node = node;
-//            this.shape = shape;
-//        }
-//
-//        public GameObject construct () {
-//            return new GameObject(model, node, shape);
-//        }
-//
-//        @Override
-//        public void dispose () {
-//            shape.dispose();
-//        }
-//    }
-
+    /**
+     * Might be needed since model is disposed at the very end separately from the body & shape.
+     */
+    public void modelDispose(){
+        model.dispose();
+    }
 }
