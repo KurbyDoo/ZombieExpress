1db195eef2c346de19381a2b34da0620a90123c4
ZleepyZleepy <alexander.hui@mail.utoronto.ca>
2025-11-17 14:54:18 -0500

    Added Timer

--- core/src/main/java/presentation/view/GameView.java
+++ core/src/main/java/presentation/view/GameView.java
@@ -51,6 +51,8 @@ public class GameView implements Viewable {
     private Label[] hotbarLabels;
     private Drawable slotNormalDrawable;
     private Drawable slotSelectedDrawable;
+    private Label timeLabel;
+    private float elapsedTime = 0;
 
     @Override
     public void createView() {
@@ -84,6 +86,7 @@ public class GameView implements Viewable {
     public void renderView() {
         float deltaTime = Gdx.graphics.getDeltaTime();
         accumulator += deltaTime;
+        elapsedTime += deltaTime;
 
         while (accumulator >= TIME_STEP) {
             accumulator -= TIME_STEP;
@@ -104,6 +107,7 @@ public class GameView implements Viewable {
         // RENDER UPDATES
         cameraController.renderCamera(alpha);
         objectRenderer.render();
+        refreshTimeLabel();
         refreshHotbarSelection();
         uiStage.act(deltaTime);
         uiStage.draw();
@@ -119,9 +123,18 @@ public class GameView implements Viewable {
         uiStage = new Stage(new ScreenViewport());
         BitmapFont uiFont = new BitmapFont();
 
+        Label.LabelStyle timeStyle = new Label.LabelStyle(uiFont, Color.WHITE);
+        timeLabel = new Label("", timeStyle);
+        timeLabel.setFontScale(2);
+        Table timeTable = new Table();
+        timeTable.setFillParent(true);
+        timeTable.top().left().padTop(10).padLeft(10);
+        timeTable.add(timeLabel);
+        uiStage.addActor(timeTable);
+        refreshTimeLabel();
+
         slotNormalDrawable = createSlotDrawable(Color.LIGHT_GRAY);
         slotSelectedDrawable = createSlotDrawable(Color.WHITE);
-
         Table hotbarTable = new Table();
 
         int HOTBAR_SIZE = 10;
@@ -171,6 +184,17 @@ public class GameView implements Viewable {
         Texture texture = new Texture(pixmap);
         pixmap.dispose();
         return new TextureRegionDrawable(new TextureRegion(texture));
+    }
+
+    private void refreshTimeLabel() {
+        if (timeLabel == null) return;
+
+        int totalSeconds = (int) elapsedTime;
+        int minutes = totalSeconds / 60;
+        int seconds = totalSeconds % 60;
+
+        String text = String.format("Time: %02d:%02d", minutes, seconds);
+        timeLabel.setText(text);
     }
 
     private void refreshHotbarSelection() {
