ff6de01ec28085b83ac286f0ae527bce4e94303c
Leobstere <jerryzh163@gmail.com>
2025-11-16 19:06:14 -0500

    Fixes null pointer issues

--- core/src/main/java/physics/GameObject.java
+++ core/src/main/java/physics/GameObject.java
@@ -6,7 +6,10 @@ import com.badlogic.gdx.physics.bullet.collision.btCollisionObject;
 import com.badlogic.gdx.physics.bullet.collision.btCollisionShape;
 import com.badlogic.gdx.utils.Disposable;
 
-public class GameObject extends ModelInstance implements Disposable {
+public class GameObject extends ModelInstance {
+
+    public Model model;
+    public btCollisionShape shape;
 
     public final btCollisionObject body;
     public boolean moving = true;
@@ -14,34 +17,37 @@ public class GameObject extends ModelInstance implements Disposable {
     public GameObject(Model model, String node, btCollisionShape shape){
         super(model, node);
         this.id = node;
+
+        this.shape = shape;
+
         body = new btCollisionObject();
         body.setCollisionShape(shape);
     }
 
-    @Override
     public void dispose () {
         body.dispose();
+        shape.dispose();
     }
 
-    static class Constructor implements Disposable {
-        public final Model model;
-        public final String node;
-        public final btCollisionShape shape;
-
-        public Constructor (Model model, String node, btCollisionShape shape) {
-            this.model = model;
-            this.node = node;
-            this.shape = shape;
-        }
-
-        public GameObject construct () {
-            return new GameObject(model, node, shape);
-        }
-
-        @Override
-        public void dispose () {
-            shape.dispose();
-        }
-    }
+//    static class Constructor implements Disposable {
+//        public final Model model;
+//        public final String node;
+//        public final btCollisionShape shape;
+//
+//        public Constructor (Model model, String node, btCollisionShape shape) {
+//            this.model = model;
+//            this.node = node;
+//            this.shape = shape;
+//        }
+//
+//        public GameObject construct () {
+//            return new GameObject(model, node, shape);
+//        }
+//
+//        @Override
+//        public void dispose () {
+//            shape.dispose();
+//        }
+//    }
 
 }
