7450dfa9d644bff2ae035d9adc85bc2487bc9cb7
ZleepyZleepy <alexander.hui@mail.utoronto.ca>
2025-11-19 07:03:57 -0500

    Added press Q to drop item

--- core/src/main/java/infrastructure/input_boundary/GameInputAdapter.java
+++ core/src/main/java/infrastructure/input_boundary/GameInputAdapter.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.utils.IntIntMap;
+import domain.entities.Player;
 
 public class GameInputAdapter extends InputAdapter {
     private final IntIntMap keys = new IntIntMap();
@@ -15,15 +16,20 @@ public class GameInputAdapter extends InputAdapter {
     private int lastMouseY = -1;
     private float currentDeltaX = 0;
     private float currentDeltaY = 0;
+    private final Player player;
 
-    public GameInputAdapter(PlayerMovementInputBoundary playerMovementInteractor) {
+    public GameInputAdapter(PlayerMovementInputBoundary playerMovementInteractor, Player player) {
         this.playerMovementInteractor = playerMovementInteractor;
+        this.player = player;
     }
 
     @Override
     public boolean keyDown(int keycode) {
-        keys.put(keycode, keycode);
-        return true;
+        if (keycode == Input.Keys.Q) {
+            player.drop();
+            return true;
+        }
+        return super.keyDown(keycode);
     }
 
     @Override
