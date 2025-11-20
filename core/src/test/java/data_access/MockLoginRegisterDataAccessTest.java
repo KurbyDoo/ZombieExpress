219d97bde23eb1e99d30272770b20692c4f10df8
Emanon-tt <tangmeiyitiang@gmail.com>
2025-11-20 13:53:30 -0500

    Save current state before Firebase integration

---
+++ core/src/test/java/data_access/MockLoginRegisterDataAccessTest.java
@@ -0,0 +1,30 @@
+package data_access;
+
+
+import UseCases.Login.LoginInputBoundary;
+import data_access.login.MockLoginRegisterDataAccess;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class MockLoginRegisterDataAccessTest {
+    @Test
+    public void testRegisterSuccess() {
+        MockLoginRegisterDataAccess dataAccess = new MockLoginRegisterDataAccess();
+        String uid = dataAccess.newUser("a@gmail.com", "password1");
+
+        assertNotNull(uid);
+        assertEquals(uid, dataAccess.login("a@gmail.com", "password1"));
+    }
+
+    @Test
+    public void testRegisterWithSameEmail() {
+        MockLoginRegisterDataAccess dataAccess = new MockLoginRegisterDataAccess();
+       dataAccess.newUser("a@gmail.com", "password1");
+       String uid2 = dataAccess.newUser("a@gmail.com","password2" );
+
+       assertNotNull(uid2);
+       // The second registration should fail, and the returned user ID should be null.
+    }
+
+}
