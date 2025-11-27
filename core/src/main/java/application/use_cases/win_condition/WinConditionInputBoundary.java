488d5b820b9cc951b75874ee2fb1bbf93d95b9bb
zaynahahmer <zaynah.ahmer@mail.utoronto.ca>
2025-11-26 21:32:18 -0500

    create use case of win condition

---
+++ core/src/main/java/application/use_cases/win_condition/WinConditionInputBoundary.java
@@ -0,0 +1,14 @@
+package application.use_cases.win_condition;
+
+/**
+ * Defines the contract for checking if the win condition has been met.
+ * The use case should handle the logic for determining the win and initiating
+ * any necessary game shutdown or state change.
+ */
+public interface WinConditionInputBoundary {
+    /**
+     * Executes the check for the win condition.
+     * @return Output data indicating if the game was won and providing a message.
+     */
+    WinConditionOutputData execute();
+}
