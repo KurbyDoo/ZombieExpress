533963ba908bf3d89a4540c1b9d3b67c0b5a0d2c
ZleepyZleepy <alexander.hui@mail.utoronto.ca>
2025-11-27 16:21:23 -0500

    Minor inventory fixes

--- core/src/main/java/domain/entities/Train.java
+++ core/src/main/java/domain/entities/Train.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.math.Vector3;
 public class Train extends Entity {
 
     private int fuel;
+    private final int MAX_FUEL = 100;
 
     public Train(Integer id, EntityType type, Vector3 position, boolean isVisible) {
         super(id, type, position, isVisible);
@@ -16,13 +17,13 @@ public class Train extends Entity {
     }
 
     public int getMaxFuel() {
-        return 100;
+        return MAX_FUEL;
     }
 
     public void addFuel(int amount) {
         this.fuel += amount;
-        if (this.fuel > 100) {
-            this.fuel = 100;
+        if (this.fuel > MAX_FUEL) {
+            this.fuel = MAX_FUEL;
         }
     }
 
